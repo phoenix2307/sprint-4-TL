01bd77798cae81c74ab264aae099b214c2474731
Alex <phoenix.trade2307@gmail.com>
2024-12-21 14:08:27 +0200

    home work task without includes is done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,8 +11,36 @@ export const App = () => {
     const themeMode = useAppSelector(selectThemeMode)
 
 // ======================================================================
+    function updateArray<T>(array: T[], element: T) {
+        if (!array.includes(element)) {
+            return [...array, element]
+        } else {
+            return [...array]
+        }
+        // code
+        // for (let i = 0; i < array.length; i++) {
+        //     if(array !== element ) {
+        //
+        //     } else {
+        //
+        //     }
+        // }
+    }
 
 
+// Строки
+    const stringArray = ['apple', 'banana', 'cherry']
+    const result1 = updateArray(stringArray, 'banana') // ['apple', 'banana', 'cherry']
+    console.log(result1)
+    const result2 = updateArray(stringArray, 'date') // ['apple', 'banana', 'cherry', 'date']
+    console.log(result2)
+
+// Числа
+    const numberArray = [1, 2, 3]
+    const result3 = updateArray(numberArray, 2) // [1, 2, 3]
+    console.log(result3)
+    const result4 = updateArray(numberArray, 4) // [1, 2, 3, 4]
+    console.log(result4)
 // ===========================================================================
     return (
         <ThemeProvider theme={getTheme(themeMode)}>
