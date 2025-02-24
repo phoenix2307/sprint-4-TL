f1c95e0c7cb2f893728d44e619f964f574add9bf
Alex <phoenix.trade2307@gmail.com>
2025-02-24 23:26:46 +0200

    corrected password validation

--- src/features/auth/lib/shemas/loginSchema.ts
+++ src/features/auth/lib/shemas/loginSchema.ts
@@ -1,16 +1,13 @@
 import {z} from 'zod'
 
-const passwordValidation = new RegExp(
-    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-);
+// const passwordValidation = new RegExp(
+//     /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+// );
 
 export const loginSchema = z.object({
     email: z.string().min(1, {message: 'Email is required'}).email({message: 'Incorrect email address'}),
     password: z.string()
-        .min(4, {message: 'Password must be at least 4 characters long'})
-        .regex(passwordValidation, {
-                message: 'Your password is not valid',
-            }),
+        .min(4, {message: 'Password must be at least 4 characters long'}),
     rememberMe: z.boolean()
 })
 export type Inputs = z.infer<typeof loginSchema>
